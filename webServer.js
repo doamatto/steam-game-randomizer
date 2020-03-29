b05f8931c3c457875535b4017e9dc90a7a39799a
doamatto <hello@doamatto.xyz>
2020-03-28 20:16:48 -0700

    Web server changes

--- webServer.js
+++ webServer.js
@@ -22,14 +22,14 @@ try {
 } catch(e) {
     console.error("The module \"express\" isn't installed. You can manually install by running \"npm i https\"");
     process.exit(e.code);
-}
+} // Make sure Express is installed
 
 try {
     require.resolve("https");
 } catch(e) {
     console.error("The module \"https\" isn't installed. You can manually install by running \"npm i https\"");
     process.exit(e.code);
-}
+} // Make sure HTTPS is installed
 
 const express = require("express");
 const https = require("https");
@@ -43,8 +43,8 @@ app.get("/", function(req, res) {
 });
 
 app.get("/r", function(req,res) {
-    var steamID = req.query.steamID;
-    https.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${config.steamKEY}&steamid=${config.steamID}&include_appinfo=1`, function(res) {
+    var steamID = req.query.steamID; // Fetch steam ID from request
+    https.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${config.steamKEY}&steamid=${steamID}&include_appinfo=1`, function(res) {
         var body = "";
         res.on("data", function(chunk) { body += chunk;});
         res.on("end", function() {
@@ -52,12 +52,12 @@ app.get("/r", function(req,res) {
             var random_game_no = Math.floor((Math.random() * data_parsed.response.game_count)); // Randomly grabs one of the games it finds in the JSON data
             var appID = data_parsed.response.games[random_game_no].appid;
             var appName = data_parsed.response.games[random_game_no].name;
-            console.log(`Here's a random game: ${appName} (${appID}). You can open it via Steam or by going to "steam://run/${appID}"`);
         });
         res.on("error", function(e) {
             console.error("Seems something is broken. Make sure your token is valid, your profile is public, and that Steam's servers aren't down. If that's all a-okay, put an issue at https://github.com/doamatto/random-steam-game-picker/issues/new detailing any errors to follow. Error report: " + e);
         });
     });
+    res.sendStatus(200, `{appid: ${appID}, appName: ${appName}}`); // Send data back to call
 });
 
 const listener = app.listen(config.web_port, function() {
