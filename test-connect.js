52ecceefaaf04c31232fe3722afbf56a47fac1f6
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-22 10:20:27 -0700

    Fixed improper usage

--- test-connect.js
+++ test-connect.js
@@ -17,11 +17,11 @@ const https = require("https");
 
 try {
   https.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${conf.steamKEY}&steamid=76561198185019113&include_appinfo=1`, function(res) {
-    res.on("error", (e => {
+    res.on("error", (e) => {
       console.error(
         `Ensure your Steam key is valid, your profile is public, and that Steam's servers aren't down. Error report: ${e}`
       );
-    }));
+    });
   });
 } catch (e) {
   console.error(`Here's a slightly more verbose log for what happened: ${e}`);
