f0988c2db2ab4dcf9bac734fd1b77ef41e10726f
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-22 10:18:59 -0700

    Changed error messages and minimized scription; reindented

--- test-connect.js
+++ test-connect.js
@@ -4,21 +4,25 @@
 //   * © 2020 doamatto. All rights reserved.
 //   
 
-const config = require("./config.json");
+const conf = require("./config.json");
 
 try {
-    require.resolve("https");
+  require.resolve("https");
 } catch(e) {
-    console.error("The module \"https\" isn't installed. You can manually install by running \"npm i https\"");
-    process.exit(e.code);
+  console.error("\"https\" isn't installed. You can do install it with \"npm i https\"");
+  process.exit(e.code);
 }
 
 const https = require("https");
 
 try {
-    https.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${config.steamKEY}&steamid=76561198185019113&include_appinfo=1`, function(res) {
-            res.on("error", function(e) { console.error("Seems something is broken. Make sure your token is valid, your profile is public, and that Steam's servers aren't down. If that's all a-okay, put an issue at https://github.com/doamatto/random-steam-game-picker/issues/new detailing any errors to follow. Error report: " + e);});
-    });
+  https.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${conf.steamKEY}&steamid=76561198185019113&include_appinfo=1`, function(res) {
+    res.on("error", (e => {
+      console.error(
+        `Ensure your Steam key is valid, your profile is public, and that Steam's servers aren't down. Error report: ${e}`
+      );
+    }));
+  });
 } catch (e) {
-    console.error(`Here's a slightly more verbose log for what happened: ${e}`);
+  console.error(`Here's a slightly more verbose log for what happened: ${e}`);
 }
